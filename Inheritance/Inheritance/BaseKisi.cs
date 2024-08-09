6a2fb8a33efbde81126fffe6c132b3c20c371539
Sefa Saraç <srcsfysf@gmail.com>
2024-08-09 13:29:10 +0300

    Add files via upload

---
+++ Inheritance/Inheritance/BaseKisi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class BaseKisi
+    {
+        // Base Sınıf için Özellikler
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        // Base Sınıf için Metot
+        public void AdSoyadYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    public class Ogrenci : BaseKisi
+    {
+        // Ogrenci Sınıfı için İlave Özellik
+        public int OgrenciNumarasi { get; set; }
+
+        // Ogrenci Sınıfı için İlave Metot
+        public void OgrenciBilgileriniYazdir()
+        {
+            Console.WriteLine($"Öğrenci Numarası: {OgrenciNumarasi}");
+            AdSoyadYazdir();  // BaseKisi sınıfından gelen metodu çağırıyoruz
+        }
+    }
+
+    public class Ogretmen : BaseKisi
+    {
+        // Ogretmen Sınıfı için İlave Özellik
+        public decimal Maas { get; set; }
+
+        // Ogretmen Sınıfı için İlave Metot
+        public void OgretmenBilgileriniYazdir()
+        {
+            Console.WriteLine($"Maaş: {Maas} TL");
+            AdSoyadYazdir();  // BaseKisi sınıfından gelen metodu çağırıyoruz
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Öğrenci Nesnesi Oluşturma ve Değer Atama
+        Ogrenci ogrenci1 = new Ogrenci();
+        ogrenci1.Ad = "Ali";
+        ogrenci1.Soyad = "Yılmaz";
+        ogrenci1.OgrenciNumarasi = 12345;
+
+        // Öğrenci Bilgilerini Yazdırma
+        Console.WriteLine("Öğrenci Bilgileri:");
+        ogrenci1.OgrenciBilgileriniYazdir();
+        Console.WriteLine();  // Satır arasında boşluk bırakmak için
+
+        // Öğretmen Nesnesi Oluşturma ve Değer Atama
+        Ogretmen ogretmen1 = new Ogretmen();
+        ogretmen1.Ad = "Ayşe";
+        ogretmen1.Soyad = "Demir";
+        ogretmen1.Maas = 7500.50m;
+
+        // Öğretmen Bilgilerini Yazdırma
+        Console.WriteLine("Öğretmen Bilgileri:");
+        ogretmen1.OgretmenBilgileriniYazdir();
+    }
+}
